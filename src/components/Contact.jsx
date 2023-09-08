93d92e8db62dee215f27bd94ad67abb975a5f561
MartinFarro2020 <martin.farro.martinez@gmail.com>
2023-09-08 13:33:17 -0500

    mail

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,27 @@
+import Mailer from "./mailer";
+
 const Contact = () => {
+
+  const handlesubmit = () => {
+   return 
+  }
+  
   return (
     <main className="bg-black mt-[100px] sm:mt-[50px]" id="contact">
-     <section className="grid" >
-      <section className="grid mt-[250px] mb-[200px] mx-auto justify-center items-center sm:mt-[100px] sm:mb-[300px] ">
-        <div className="grid text-center w-[250px] h-[400px] bg-zinc-800 rounded-3xl items-center -mt-[150px] sm:w-[890px] sm:h-[370px]">
-          <p className="text-[35px] font-semibold text-[#9BA4ED] mx-auto sm:text-[48px] sm:text-white">Contact me!</p>
-          <button className="text-white border-2 border-white w-[95px] rounded-md mx-[80px] sm:mx-auto">Click Here</button>
-        </div>  
-        <div className="absolute h-[28px] w-[97px] sm:[url(/images/Nube_desk_cinco.png)] sm:w-[300px] sm:h-[500px]"></div>
+      <section className="grid">
+        <section className="grid mt-[250px] mb-[200px] mx-auto justify-center items-center sm:mt-[300px] sm:mb-[300px] ">
+          <div className="grid text-center w-[250px] h-[1000px] bg-zinc-800 rounded-3xl items-center -mt-[150px] sm:w-[890px] sm:h-[370px]">
+            <p className="text-[35px] font-semibold text-[#9BA4ED] mx-auto sm:text-[48px] sm:text-white">
+              Contact me!
+            </p>
+            <div>
+              <Mailer />
+            </div>
+          </div>
+          <div className="absolute h-[28px] w-[97px] sm:[url(/images/Nube_desk_cinco.png)] sm:w-[300px] sm:h-[500px]"></div>
+        </section>
       </section>
-    </section>
     </main>
-    
-  )
-}
-export default Contact
+  );
+};
+export default Contact;
---
+++ src/components/Mailer.jsx
@@ -0,0 +1,27 @@
+import  emailjs  from "emailjs-com";
+
+const Mailer = () => {
+
+    function sendEmail (e){
+        e.preventDefault();
+        emailjs.sendForm('service_ptn65ub','template_ib8rs48',e.target).then(res=>{ console.log(res);}).catch(err=> console.log(err));
+    }
+
+  return (
+    <div className="container border" style={{marginTop:"50px", width:"50%",backgroundPosition:"center",backgroundSize:"cover"}}>
+        <h1>Contact me!</h1>
+        <form className="text-white flex flex-col" onSubmit={sendEmail}>
+            <label className="">Name</label>
+            <input type="text" name="name" className="form-control"/>
+
+            <label>Email</label>
+            <input type="email" name="user_email" className="form-control"/>
+
+            <label>Messaje</label>
+            <textarea name="message" rows="4" className="form-control"/>
+            <input type="submit" value="Send" className="form-control btn btn-primary" style={{marginTop:"30px"}}/>
+        </form>
+    </div>
+  );
+};
+export default Mailer
