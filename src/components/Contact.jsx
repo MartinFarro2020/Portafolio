a8aba6bb76debb4b84b3e16c8e2aab94cf68aa33
MartinFarro2020 <martin.farro.martinez@gmail.com>
2023-09-08 15:41:54 -0500

    first commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,12 +9,14 @@ const Contact = () => {
   return (
     <main className="bg-black mt-[100px] sm:mt-[50px]" id="contact">
       <section className="grid">
-        <section className="grid mt-[250px] mb-[200px] mx-auto justify-center items-center sm:mt-[300px] sm:mb-[300px] ">
+        <section className="grid mt-[250px] mb-[200px] mx-auto justify-center items-center sm:mt-[240px] sm:mb-[300px] ">
           <div className="grid text-center w-[250px] h-[200px] bg-zinc-800 rounded-3xl items-center -mt-[150px] sm:w-[890px] sm:h-[570px]">
             <p className="text-[35px] font-semibold text-[#9BA4ED] mx-auto sm:text-[48px] sm:text-white">
               Contact me!
             </p>
-            
+            <div>
+              <Mailer />
+            </div>
           </div>
           <div className="h-[28px] w-[97px] sm:[url(/images/Nube_desk_cinco.png)] sm:w-[300px] sm:h-[500px]"></div>
         </section>
